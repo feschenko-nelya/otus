df605f03156be6eb81997caa1125162ce58ca752
Nelya Feshchenko <vishenka8@list.ru>
2023-02-27 22:09:44 +0300

    Step 1 - Show build number
    Base files.
    First task to show build number.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "version.h"
+
+int main()
+{
+    std::cout << "Build: " << VERSION_N << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    
+    return 1;
+}
