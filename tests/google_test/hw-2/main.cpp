ef409425bb94e07cd12c61f4ff597d5aaef6c6c2
Nelya <vishenka8@list.ru>
2023-05-01 13:58:37 +0300

    Add google-test for hw-2

---
+++ tests/google_test/hw-2/main.cpp
@@ -0,0 +1,26 @@
+#include "googletest/googlemock/include/gmock/gmock.h"
+#include "googletest/googletest/include/gtest/gtest.h"
+#include <string>
+#include "../../../utils/utils.h"
+
+TEST(TestGroup, SubTest_1)
+{
+    ASSERT_TRUE(globalGetVersion() != 0);
+}
+
+TEST(TestGroup, SubTest_2)
+{
+    ASSERT_FALSE(globalGetVersion() < 0);
+}
+
+TEST(TestGroup, SubTest_3)
+{
+    ASSERT_TRUE(globalGetVersion() > 0);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
